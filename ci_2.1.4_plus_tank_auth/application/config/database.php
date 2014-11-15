c3879b93a7b512fb536141eb5d1c18baf813dcb7
techrepublik <joshlorilla@gmail.com>
2014-11-16 02:03:08 +0800

    db config

--- ci_2.1.4_plus_tank_auth/application/config/database.php
+++ ci_2.1.4_plus_tank_auth/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'julietaspension';
+$db['default']['password'] = 'P@ssw0rd';
+$db['default']['database'] = 'julietas_res';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
