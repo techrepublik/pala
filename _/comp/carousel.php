92229beb852767c677c1d283b65bf8dfd25efb51
techrepublik <josh.lorilla@gmail.com>
2014-11-17 22:47:29 +0800

    carousel update

--- _/comp/carousel.php
+++ _/comp/carousel.php
@@ -2,68 +2,43 @@
 <div class="slide_wrapper">
 	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
 		<!-- Wrapper for slides -->
-		<div class="carousel-inner" role="listbox">
+		<div class="carousel-inner carousel-caption-style" role="listbox">
 			<div class="item active">
 				<img src="_/images/slide1.jpg" alt="Slide 1">
 				<div class="carousel-caption">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<p>Guest Receiving Area..</p>
 				</div>
 			</div>
 			<div class="item">
 				<img src="_/images/slide2.jpg" alt="Slide 2">
 				<div class="carousel-caption">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				</div>
-			</div>
-			<div class="item">
-				<img src="_/images/slide3.jpg" alt="Slide 3">
-				<div class="carousel-caption">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<p>Coffee Shop and Dining Hall..</p>
 				</div>
 			</div>
 			<div class="item">
 				<img src="_/images/slide4.jpg" alt="Slide 4">
 				<div class="carousel-caption">
-					<p>This is slide 4</p>
+					<p>Spacious Parking Space..</p>
 				</div>
 			</div>
 			<div class="item">
 				<img src="_/images/slide6.jpg" alt="Slide 6">
 				<div class="carousel-caption">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<p>Julieta's Rooms..</p>
 				</div>
 			</div>
 		</div>
 
 		<!-- Controls -->
-		<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+		<!--<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
 			<span class="glyphicon glyphicon-chevron-left"></span>
 			<span class="sr-only">Previous</span>
 		</a>
 		<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
 			<span class="glyphicon glyphicon-chevron-right"></span>
 			<span class="sr-only">Next</span>
-		</a>
+		</a>-->
+
 		<div id="booking-panel" class="panel panel-default">
 			<div class="panel-heading">Welcome and Book Now!</div>
 			<div class="panel-body">
@@ -88,14 +63,14 @@
 							</span>
 						</div>
 					</div>
-					<div class="form-group col col-sm-6">
+					<div class="form-group col col-xs-6">
 						<div class="row input-group input-group-sm">
-							<input type="text" name="single_room" id="single_room" class="form-control" placeholder="Single rooms" />
+							<input type="text" name="single_room" id="single_room" class="form-control" placeholder="Double-Sized Bed" />
 						</div>
 					</div>
-					<div class="form-group col col-sm-6 pull-right">
+					<div class="form-group col col-xs-6 pull-right">
 						<div class="row input-group input-group-sm">
-							<input type="text" name="double_room" id="double_room" class="form-control" placeholder="Double rooms" />
+							<input type="text" name="double_room" id="double_room" class="form-control" placeholder="Twin Bed" />
 						</div>
 					</div>
 					<br/>
